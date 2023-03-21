3a2605fc24709e1f5d6e3ecd35668f057cdd69b6
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-03-21 20:30:16 +0330

    fetch users from firebase databsae

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,32 @@
-{/* <div class="user d-flex justify-content-around align-items-center p-3">
-<figure class="mb-0 rounded-circle overflow-hidden">
+'use strict';
+
+const $ = document;
+
+function fetchUsersFromFirebase() {
+  fetch('https://usercrud-428ab-default-rtdb.firebaseio.com/users.json')
+    .then((res) => {
+      if (res.status === 200) return res.json();
+    })
+    .then((users) => {
+      addUsers(users);
+    });
+}
+
+function addUsers(users) {
+  let usersFragment = document.createDocumentFragment();
+  for (const user of Object.values(users)) {
+    usersFragment.appendChild(createUserElement(user));
+  }
+  $.querySelector('main').append(usersFragment);
+}
+
+function createUserElement(user) {
+  let userElem = $.createElement('div');
+  userElem.className =
+    'user d-flex justify-content-around align-items-center p-3';
+  userElem.insertAdjacentHTML(
+    'afterbegin',
+    `<figure class="mb-0 rounded-circle overflow-hidden">
   <img
     class="img-fluid"
     src="../img/male_avatar.png"
@@ -7,17 +34,21 @@
   />
 </figure>
 <section class="user_info ps-4">
-  <p class="name mb-0 text-light">ali t.nazari</p>
-  <p class="email mb-0 text-light">ali@gmail.com</p>
-  <p class="pass mb-0 text-light">@Li44272815</p>
+  <p class="name mb-0 text-light">${user.name}</p>
+  <p class="email mb-0 text-light">${user.email}</p>
+  <p class="pass mb-0 text-light">${user.password}</p>
 </section>
 <section class="buttons d-flex w-25">
   <button class="btn btn-secondary me-2">delete</button>
   <button class="btn btn-secondary">edit</button>
-</section>
-</div> */}
+</section>`
+  );
+  return userElem;
+}
 
-'use strict';
 
-const $ = document;
+
+window.addEventListener('load', () => {
+  fetchUsersFromFirebase();
+});
 
--- src/js/register.js
+++ src/js/register.js
@@ -1,4 +1,3 @@
-// location.href = "index.html";
 'use strict';
 
 const $ = document;
@@ -16,7 +15,23 @@ function addUserToFirebase(newUser) {
     },
     body: JSON.stringify(newUser),
   }).then((res) => {
-    console.log(res);
+    if (res.status === 200) {
+      Toast.fire({
+        icon: 'success',
+        title: `${newUser.name} added successfully`,
+        didDestroy: () => {
+          location.href = 'index.html';
+        },
+      });
+    }
+    if(res.status === 401) {
+        Swal.fire({
+            icon: 'error',
+            title: '401 unauthorized',
+            text: 'Something went wrong!',
+            footer: '<a href="https://firebase.google.com/docs/cloud-messaging/auth-server">Why do I have this issue?</a>'
+          })
+    }
   });
 }
 
@@ -28,4 +43,16 @@ form.addEventListener('submit', (e) => {
     password: password.value,
   };
   addUserToFirebase(newUser);
+});
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer);
+    toast.addEventListener('mouseleave', Swal.resumeTimer);
+  },
 });
