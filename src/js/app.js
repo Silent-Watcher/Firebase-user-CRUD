34917b7a4aa8fd6f3a6c169ae54587ae4694f6ff
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-03-23 19:46:08 +0430

    change button colors

--- src/js/app.js
+++ src/js/app.js
@@ -63,8 +63,8 @@ async function removeUser(event) {
     text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
+    confirmButtonColor: '#404040',
+    cancelButtonColor: '#232323',
     confirmButtonText: 'Yes, delete it!',
   }).then((result) => {
     if (result.isConfirmed) {
